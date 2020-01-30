0d89f7ca526cb342b53b85ce0a9d589c4010cc0a
Felipe Antonio <felipe.antonio.santos@ccc.ufcg.edu.br>
2020-01-30 12:34:34 -0300

    adição do cadastro

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+//criação do app
+const app = express();
+
+//config requisições json e URL
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//rotas
+require('./controllers/authController')(app);
+
+//porta
+app.listen(5000);
